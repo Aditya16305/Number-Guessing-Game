d21d269105295758f079f9bc6e44ccc70c86e8fb
Aditya <61423116+Aditya16305@users.noreply.github.com>
2021-12-11 11:34:49 +0530

    Update projc96.py

--- projc96.py
+++ projc96.py
@@ -1,26 +1,32 @@
+from os import startfile
 import random
 
 i = 0
 
 num1 = random.randint(0, 9)
 
+# print(num1)
+
+state = 'start'
+
 while(i < 3):
-    
-    i+=1
-    
+
+    i += 1
+
     guess = int(input('Guess: '))
-    
+
     if(guess > num1):
         print('Too high!')
-        
-    
+
     elif(guess < num1):
         print('Too low!')
-        
-        
+
     elif(guess == num1):
         print('You guessed it!')
-        i = 4
-    
-if(i == 3):
+        state = 'win'
+
+    if state == 'win':
+        break
+
+if(not i < 3 and state != 'win'):
     print('You lost!')
